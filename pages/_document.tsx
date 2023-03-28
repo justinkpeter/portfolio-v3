dabd871036f83e9ffd3eaf83d94bda36a0b6d229
Justin <justinkmpeter@gmail.com>
2023-03-28 17:16:15 -0500

    Adding three.js scripts to the head of the HTML documents

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from "next/script";
+import * as React from "react";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <Script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js" strategy="beforeInteractive" />
+          <Script src="https://cdn.jsdelivr.net/npm/react-three-fiber/dist/index.js" strategy="beforeInteractive" />
+          <Script src="https://cdn.jsdelivr.net/npm/three/examples/js/loaders/GLTFLoader.js" strategy="beforeInteractive" />
+      </Head>
       <body>
         <Main />
         <NextScript />
