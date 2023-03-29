e8366d8c61f303b844d8d0ce1f5de85e2dbfc621
Justin <justinkmpeter@gmail.com>
2023-03-29 18:33:21 -0500

    Making ArcText component more responsive on mobile

--- components/ArcText.tsx
+++ components/ArcText.tsx
@@ -3,7 +3,7 @@ import React, {useEffect} from 'react';
 
 
 
-export const ArcText = () => {
+export  const ArcText = () => {
     useEffect(() => {
         const text = document.querySelector<HTMLElement>('#circle-text');
         if(text === null){
@@ -12,9 +12,7 @@ export const ArcText = () => {
         }
         else if(text && text.textContent){
             text.innerHTML = text.textContent.replace(/\S/g,
-                // "<span class='letter inline-block  text-xs sm:text-lg uppercase origin-[0_65px] sm:origin-[0_10rem] md:origin-[0_11rem] lg:origin-[0_12rem] xl:origin-[0_12rem] 2xl:origin-[0_15rem] absolute sm:top-12 md:top-[4vh] lg:top-[8vh] 2xl:top-[12.5vh] sm:left-1/2 sm:right-1/2 font-medium z-50'>$&</span>");
-                // "<span class='letter inline-block  text-xs sm:text-lg uppercase origin-[0_calc(20vh)] absolute sm:top-12 md:top-[4vh] lg:top-[8vh] 2xl:top-[12.5vh] sm:left-1/2 sm:right-1/2 font-medium z-50'>$&</span>");
-                "<span class='letter inline-block text-xs sm:text-lg uppercase origin-[0_calc(20vh)] absolute sm:top-[calc(10vh)] sm:left-1/2 sm:right-1/2 font-light z-50'>$&</span>");
+                "<span class='letter inline-block text-xs sm:text-lg uppercase origin-[0_65px] sm:origin-[0_calc(20vh)] absolute top-[1vh] sm:top-[calc(10vh)] sm:left-1/2 right-[10vh] sm:right-1/2 font-light z-50'>$&</span>");
             const element = document.querySelectorAll<HTMLElement>('.letter');
             if(element === null)
                 return;
@@ -36,5 +34,3 @@ export const ArcText = () => {
 
     return (<h4 id={'circle-text'} className={'bg-blue-200 circle-text text-[9px]'}> creator  ✶  designer  ✶  coder ✶ </h4>);
 }
-
-export default ArcText;
