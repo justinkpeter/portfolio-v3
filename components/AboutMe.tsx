4764b8980a8908f9fc86de10b8b2d485d4caff83
Justin <justinkmpeter@gmail.com>
2023-04-03 15:29:25 -0500

    modifying text breakpoints

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -8,7 +8,7 @@ export const AboutMe = () => {
 
     return (
         <>
-            <section id={'about'} className={'relative h-fit  my-24 pt-14 sm:my-36'}>
+            <section id={'about'} className={'relative h-fit  my-24 pt-14 sm:my-36 md:my-12'}>
                 <Divider showLabels={true} labels={dividerLabels}/>
                 <div className={'text-4xl md:text-5xl lg:text-7xl relative font-bold font-panchang uppercase pt-16 lg:pt-28 sm:pl-[10rem] lg:pl-[16rem] h-fit'}>
                     <div className={'flex flex-col relative space-y-3 w-full'}>
@@ -17,11 +17,11 @@ export const AboutMe = () => {
                         <h1> coding, </h1>
                         <h1> music. </h1>
                     </div>
-                    <div className={'relative w-full lg:w-3/4 h-fit text-xl flex flex-col sm:space-y-6 sm:flex-row py-0 space-y-6 sm:py-6 lg:py-0 lg:space-y-0 normal-case font-cabinet font-medium mt-8 sm:mt-10 '}>
-                        <div className={'w-full font-bold lg:w-1/2 uppercase text-xl md:text-2xl lg:text-3xl  '}>
+                    <div className={'relative w-full lg:w-3/4 h-fit text-xl flex flex-col sm:space-y-6 xl:flex-row py-0 space-y-6 sm:py-6 xl:py-0 lg:space-y-6 xl:space-y-0 normal-case font-cabinet font-medium mt-8 sm:mt-10 '}>
+                        <div className={'w-full font-bold xl:w-1/2 uppercase text-xl md:text-2xl lg:text-3xl  '}>
                             <h5>The Holy Trinity to my life</h5>
                         </div>
-                        <div className={' w-full sm:w-3/4 lg:w-1/2 flex flex-col text-lg sm:text-xl md:text-2xl relative'}>
+                        <div className={' w-full sm:w-3/4 md:w-full xl:w-1/2 flex flex-col text-lg sm:text-xl md:text-2xl relative'}>
                             <p className={'w-full'}>
                                 For me, coding is more than just combining ones and zeros. It’s about fostering community and pushing the
                                 boundaries of our imaginations.
