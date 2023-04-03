8a75108bc6be32ec3c0af60d32a5351ce900afaf
Justin <justinkmpeter@gmail.com>
2023-04-03 14:41:49 -0500

    Added gsap and circletype CDN

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,9 +6,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-          <Script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js" strategy="beforeInteractive" />
-          <Script src="https://cdn.jsdelivr.net/npm/react-three-fiber/dist/index.js" strategy="beforeInteractive" />
-          <Script src="https://cdn.jsdelivr.net/npm/three/examples/js/loaders/GLTFLoader.js" strategy="beforeInteractive" />
+          <Script src={'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'} strategy={'beforeInteractive'} />
+          <Script src={'https://cdn.jsdelivr.net/npm/react-three-fiber/dist/index.js'} strategy={'beforeInteractive'} />
+          <Script src={'https://cdn.jsdelivr.net/npm/three/examples/js/loaders/GLTFLoader.js'} strategy={'beforeInteractive'} />
+          <Script src={'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.6/gsap.min.js'} strategy={'beforeInteractive'} />
+          <Script src={'https://cdn.jsdelivr.net/npm/circletype@2.3.0/dist/circletype.min.js'} strategy={'beforeInteractive'}/>
       </Head>
       <body>
         <Main />
