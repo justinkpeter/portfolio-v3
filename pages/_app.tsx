f136a2b69683ead32b47504964a5ab5f687905cb
Justin <justinkmpeter@gmail.com>
2023-04-03 14:48:46 -0500

    custom cursor component using gsap and circletype

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {CustomCursor} from "@/components/CustomCursor";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Component {...pageProps} />
+        <CustomCursor/>
+      </>
+  )
 }
