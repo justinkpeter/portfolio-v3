9c0be285a5799f5ebf162fae14d328543efb127b
Justin <justinkmpeter@gmail.com>
2023-04-17 10:33:59 -0500

    adjusted width on email tag

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -14,7 +14,7 @@ export const Contact = () => {
                     <div
                         data-cursor-text={'EMAIL ME *'}
                         data-cursor-text-repeat="3"
-                        className={'cursor-hover-item text-5xl sm:text-6xl md:text-7xl xl:hidden lg:text-8xl  relative font-semibold font-panchang uppercase my-[14vw] xl:pl-[16rem]  break-words underline'}>
+                        className={'cursor-hover-item  text-5xl sm:text-6xl md:text-7xl xl:hidden lg:text-8xl  relative font-semibold font-panchang uppercase my-[14vw] xl:pl-[16rem]  break-words underline'}>
                         <a href={`mailto: ${userData.email}?subject=Hi Justin!`}> hello@</a><br/>
                         <a href={`mailto: ${userData.email}?subject=Hi Justin!`}> justin</a><br/>
                         <a href={`mailto: ${userData.email}?subject=Hi Justin!`}> peter</a><br/>
@@ -23,7 +23,7 @@ export const Contact = () => {
                     <div
                         data-cursor-text={'EMAIL ME *'}
                         data-cursor-text-repeat="4"
-                        className={'cursor-hover-item  hidden xl:flex text-5xl sm:text-5xl lg:text-8xl lg:flex-col relative font-bold font-panchang uppercase my-24  sm:break-words lg:break-normal underline'}>
+                        className={'cursor-hover-item w-fit hidden xl:flex text-5xl sm:text-5xl lg:text-8xl lg:flex-col relative font-bold font-panchang uppercase my-24  sm:break-words lg:break-normal underline'}>
                         <a href={`mailto: ${userData.email}?subject=Hi Justin!`}> hello@justin</a>
                         <a href={`mailto: ${userData.email}??subject=Hi Justin!`}> peter.dev </a><br/>
                     </div>
